0f6b9585b5a02a63eec1de13a65c347c44ff79ec
idleway <rtrtr82@gmail.com>
2021-10-21 20:12:32 +0500

    change bot command help list

--- internal/app/commands/education/answer/command_help.go
+++ internal/app/commands/education/answer/command_help.go
@@ -10,7 +10,7 @@ func (c *AnswerCommander) Help(inputMessage *tgbotapi.Message) error {
 			"/list__education__answer - answer list\n"+
 			"/get__education__answer {id} - get answer by id\n"+
 			"/delete__education__answer {id} - delete answer by id\n"+
-			"/add__education__answer {json} - add new answer, id will be omitted\n"+
+			"/new__education__answer {json} - add new answer, id will be omitted\n"+
 			"/edit__education__answer {json} - edit answer\n",
 	)
 	_, err := c.bot.Send(message)
